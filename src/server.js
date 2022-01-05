a416bc37050767aa718a2eaeec50e9fe5c0dcc91
Kim Hyewon <gpdnjs861@naver.com>
2022-01-05 21:45:11 +0900

    :bulb: study middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,27 @@ const app = express();
 
 //how to response get request
 
-const handleLogin = (req, res) => {
-  return res.send('This is login page');
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
-app.get('/', (req, res) => {
+const privateMiddleware = (req, res, next) => {
+  if (req.url === '/protected') {
+    return res.send('<h1>Not Allowed</h1>');
+  }
+  console.log("Hi You'll pass");
+  next();
+};
+const handleHome = (req, res) => {
   return res.end();
-});
-app.get('/login', handleLogin);
+};
+const handleProtected = (req, res) => {
+  return res.send('Welcome private page');
+};
+app.use(logger);
+app.use(privateMiddleware);
+app.get('/', handleHome);
+app.get('/protected', handleProtected);
 // application starts to listen request
 const handleListening = () => console.log(`server on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
