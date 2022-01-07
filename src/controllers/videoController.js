6ba00181a181cae463c1cd255db83f1981babc69
Kim Hyewon <gpdnjs861@naver.com>
2022-01-08 01:51:45 +0900

    :bulb: Cleaning the code  study

---
+++ src/controllers/videoController.js
@@ -0,0 +1,5 @@
+export const trending = (req, res) => res.send('Home');
+export const search = (req, res) => res.send('Search');
+export const watch = (req, res) => res.send('Watch Video');
+export const edit = (req, res) => res.send('Edit video');
+export const remove = (req, res) => res.send('Remove video');
