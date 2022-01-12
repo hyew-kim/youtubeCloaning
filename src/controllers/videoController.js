9071439144533694556758b9829d928e31a20f16
Kim Hyewon <gpdnjs861@naver.com>
2022-01-13 02:07:16 +0900

    :bulb: pug grammer study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,31 @@
-const fakeUser = {
-  username: 'hyewkim',
-  loggedIn: true,
-};
+const videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Third Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+];
 
-export const trending = (req, res) => res.render('home', { pageTitle: 'Home', fakeUser: fakeUser });
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home', videos });
 export const see = (req, res) => res.render('watch');
 export const edit = (req, res) => res.send('Edit video');
 export const search = (req, res) => res.send('Search');
