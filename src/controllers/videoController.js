3aaae704e473ce0ce34d7498843ab081af137d12
Kim Hyewon <gpdnjs861@naver.com>
2022-01-14 02:07:59 +0900

    :bulb: response at post request from form study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-const videos = [
+let videos = [
   {
     title: 'First Video',
     rating: 5,
@@ -12,22 +12,55 @@ const videos = [
     rating: 5,
     comments: 2,
     createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
+    views: 1,
+    id: 2,
   },
   {
     title: 'Third Video',
     rating: 5,
     comments: 2,
     createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
+    views: 9,
+    id: 3,
   },
 ];
 
 export const trending = (req, res) => res.render('home', { pageTitle: 'Home', videos });
-export const see = (req, res) => res.render('watch');
-export const edit = (req, res) => res.send('Edit video');
+export const watch = (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const title = req.body.title;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render('upload', { pageTitle: 'Upload' });
+};
+export const postUpload = (req, res) => {
+  //here we will add a video to the videos array
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: '2 minutes ago',
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect('/');
+  //broser는 post request로 /video/upload로 가고 get request로 /로 간다
+  //get("/video/upload/")-> form submit -> post request ("video/upload")-> redirect("/") (home page get해오라는 의미임!!)-> trending controller call 즉, get request ("/")
+};
 export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload');
 export const remove = (req, res) => res.send('Remove video');
--- src/routes/videoRouter.js
+++ src/routes/videoRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { upload, edit, remove, see } from '../controllers/videoController';
+import { getUpload, postUpload, getEdit, remove, watch, postEdit } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/upload', upload);
-videoRouter.get('/:id', see);
-videoRouter.get('/:id/edit', edit);
+videoRouter.get('/upload', getUpload);
+videoRouter.post('/upload', postUpload);
+videoRouter.get('/:id', watch);
+videoRouter.route('/:id/edit').get(getEdit).post(postEdit);
 videoRouter.get('/:id/remove', remove);
+
 export default videoRouter;
+//controller -> route 순서로 작업하면 편한듯?
